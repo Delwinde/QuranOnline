941e86d7c2fe9b0e993f6f54306a4181bc7261bd
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-12-09 18:13:47 +0100

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function getChapterValue()
+{
+    var chapterValue = document.getElementById("chapter").value;
+    console.log(chapterValue);
+}
+
+function getReciterValue()
+{
+    var reciterValue = document.getElementById("reciter").value;
+    console.log(reciterValue);
+}
